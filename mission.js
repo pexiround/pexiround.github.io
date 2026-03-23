2b4b30e6e373436f82df7c30d186fda874981e22
pexiround <258477744+pexiround@users.noreply.github.com>
2026-03-23 20:37:57 +0100

    Update mission.js

--- mission.js
+++ mission.js
@@ -1,1 +1,20 @@
+const missionContent = document.getElementById('mission-content');
 
+const missionText = `
+[OPERATION: BLACKOUT]
+Target Domain: auth.domain-secure.net
+Objective: Inject malware and slow server
+`;
+
+function typeMission(text, element) {
+  let i = 0;
+  function type() {
+    if(i < text.length) {
+      element.innerHTML += text[i++];
+      setTimeout(type, 30);
+    }
+  }
+  type();
+}
+
+typeMission(missionText, missionContent);
