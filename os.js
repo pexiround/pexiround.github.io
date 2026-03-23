688aa70490417f2526669057b739f47a00b116aa
pexiround <258477744+pexiround@users.noreply.github.com>
2026-03-23 20:37:13 +0100

    Update os.js

--- os.js
+++ os.js
@@ -1,1 +1,39 @@
+function openApp(appId) {
+  const app = document.getElementById(appId);
+  if(app.style.display === "block") return;
+  app.style.display = "block";
+}
 
+// Make windows draggable
+const windows = document.querySelectorAll('.window');
+windows.forEach(win => {
+  const titleBar = win.querySelector('.title-bar');
+  titleBar.onmousedown = function(event) {
+    let shiftX = event.clientX - win.getBoundingClientRect().left;
+    let shiftY = event.clientY - win.getBoundingClientRect().top;
+
+    win.style.position = 'absolute';
+    win.style.zIndex = 1000;
+    document.body.append(win);
+
+    function moveAt(pageX, pageY) {
+      win.style.left = pageX - shiftX + 'px';
+      win.style.top = pageY - shiftY + 'px';
+    }
+
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    win.onmouseup = function() {
+      document.removeEventListener('mousemove', onMouseMove);
+      win.onmouseup = null;
+    };
+  };
+
+  titleBar.ondragstart = function() {
+    return false;
+  };
+});
